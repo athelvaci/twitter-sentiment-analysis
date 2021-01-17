2d58b7b23c0eca7d51f7dc642303464440848e0b
Ahmet Tufan Helvaci <athelvaci@gmail.com>
2021-01-17 03:26:12 +0300

    code formatted.

--- API_KEYS.py
+++ API_KEYS.py
@@ -1,4 +1,4 @@
-api_key = 'uwkvOlx0V5whMVHQ1q4z6p01f'
-api_secret_key = '5KFLiLmcCZztrYD5Lg0RrRcEbOAgja1g3H2SP9CwMkGHHsBblu'
-access_token = '754082263-W7XLWVws9lKKhLHbCt9x0jCacql3MHPyAZoICjyt'
-access_token_secret = '1yY29odmyX2Job94U52laPOalp4FWZ8d47EetYWYnQeWc'
+api_key = 'xxx'
+api_secret_key = 'xxx'
+access_token = 'xxx'
+access_token_secret = 'xxx'
