131caaf406a70eb9c0099e7174bf0acb2a7af2fe
Onat Uner <onat_uner@hotmail.com>
2021-01-25 22:08:16 +0300

    Removed unnecessary code and added comments

--- source3.py
+++ source3.py
@@ -13,92 +13,36 @@ auth = tweepy.OAuthHandler(api_key, api_secret_key)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
-def percentage(part,whole):
-    return 100 * float(part)/float(whole)
-
-
+# Search parameters
 keyword = "Cyberpunk"
-noOfTweet = 10
+noOfTweet = 100
 
 # Set the end date
 today = datetime.date.today()
-end = today - datetime.timedelta(days = 3)
+end = today - datetime.timedelta(days = 6)
 end_verbose = end.strftime('%A') + ', ' + end.strftime("%B") + ' ' + end.strftime("%d") + ', ' + end.strftime("%Y")
 
 # Search for tweets
 tweets = tweepy.Cursor(api.search, q = keyword, until = end).items(noOfTweet)
 
-positive = 0
-negative = 0
-neutral = 0
-polarity = 0
+# Get the text values of the tweets for later analysis
 tweet_list = []
-neutral_list = []
-negative_list = []
-positive_list = []
-
 for tweet in tweets:
-    tweet_list.append(tweet.text)
-    analysis = TextBlob(tweet.text)
-    score = SentimentIntensityAnalyzer().polarity_scores(tweet.text)
-    neg = score['neg']
-    neu = score['neu']
-    pos = score['pos']
-    comp = score['compound']
-    polarity += analysis.sentiment.polarity
-    
-    if neg > pos:
-        negative_list.append(tweet.text)
-        negative += 1
-    elif pos > neg:
-        positive_list.append(tweet.text)
-        positive += 1
-    elif pos == neg:
-        neutral_list.append(tweet.text)
-        neutral += 1
-        
-positive = percentage(positive, noOfTweet)
-negative = percentage(negative, noOfTweet)
-neutral = percentage(neutral, noOfTweet)
-polarity = percentage(polarity, noOfTweet)
-positive = format(positive, '.1f')
-negative = format(negative, '.1f')
-neutral = format(neutral, '.1f')
+    tweet_list.append(tweet.text)    
 
 tweet_list = pd.DataFrame(tweet_list)
-neutral_list = pd.DataFrame(neutral_list)
-negative_list = pd.DataFrame(negative_list)
-positive_list = pd.DataFrame(positive_list)
-print("total number: ", len(tweet_list))
-print("positive number: ", len(positive_list))
-print("negative number: ", len(negative_list))
-print("neutral number: ", len(neutral_list))
-
-#Creating PieCart
-labels = ['Positive [' + str(positive) + '%]' , 'Neutral [' + str(neutral) + '%]', 'Negative [' + str(negative) + '%]']
-sizes = [positive, neutral, negative]
-colors = ['yellowgreen', 'blue','red']
-patches, texts = plt.pie(sizes,colors=colors, startangle = 90)
-plt.style.use('default')
-plt.legend(labels)
-plt.title("Sentiment Analysis Result for keyword=  " + keyword + "" )
-plt.axis('equal')
-plt.show()
-
-tw_list = pd.DataFrame(tweet_list)
-
 tweet_list.drop_duplicates(inplace = True)
 
 tw_list = pd.DataFrame(tweet_list)
 tw_list["text"] = tw_list[0]
 
-#Cleaning Text (RT, Punctuation etc)
+# Clean the text data of the tweets (retweets, punctuation etc)
 remove_rt = lambda x: re.sub('RT @\w+: '," ",x)
 rt = lambda x: re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)"," ",x)
 tw_list["text"] = tw_list.text.map(remove_rt).map(rt)
 tw_list["text"] = tw_list.text.str.lower()
 
-#Calculating Negative, Positive, Neutral and Compound values
+# Do sentiment analysis on the tweets
 tw_list[['polarity', 'subjectivity']] = tw_list['text'].apply(lambda Text: pd.Series(TextBlob(Text).sentiment))
 for index, row in tw_list['text'].iteritems():
     score = SentimentIntensityAnalyzer().polarity_scores(row)
@@ -118,32 +62,35 @@ for index, row in tw_list['text'].iteritems():
     tw_list.loc[index, 'compound'] = comp
 
 
-#Creating new data frames for all sentiments (positive, negative and neutral)
+# Create new data frames for all sentiments
 tw_list_negative = tw_list[tw_list["sentiment"] == "negative"]
 tw_list_positive = tw_list[tw_list["sentiment"] == "positive"]
 tw_list_neutral = tw_list[tw_list["sentiment"] == "neutral"]
 
 
-#Function for count_values_in single columns
-def count_values_in_column(data,feature):
+# Function to count the values in the data frame
+def count_values_in_column(data, feature):
     total = data.loc[:,feature].value_counts(dropna = False)
     percentage = round(data.loc[:,feature].value_counts(dropna = False, normalize = True) * 100, 2)
     return pd.concat([total,percentage], axis = 1, keys = ['Total', 'Percentage'])
 
 
-#Count_values for sentiment
-count_values_in_column(tw_list,"sentiment")
+# Create the data for the pie chart
+piechart = count_values_in_column(tw_list, "sentiment")
+names = piechart.index
+size = piechart["Percentage"]
 
-
-# create data for Pie Chart
-pichart = count_values_in_column(tw_list, "sentiment")
-names = pichart.index
-size = pichart["Percentage"]
-
-# Create a circle for the center of the plot
+# Create  the pie chart with an empty middle area
 my_circle = plt.Circle( (0,0), 0.7, color = 'white')
 plt.pie(size, labels = names, colors=['green', 'blue', 'red'])
 p = plt.gcf()
 p.gca().add_artist(my_circle)
-plt.title("Sentiment Analysis for " + keyword + " on " + end_verbose )
+plt.title("Sentiment Analysis for " + keyword + " on " + end_verbose)
 plt.show()
+
+# Print the numerical data to the console
+print("Sentiment Analysis for " + keyword + " on " + end_verbose)
+print("total number: ", len(tw_list))
+print("positive number: ", len(tw_list_positive))
+print("negative number: ", len(tw_list_negative))
+print("neutral number: ", len(tw_list_neutral))
